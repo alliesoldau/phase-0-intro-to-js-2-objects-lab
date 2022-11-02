c6c4664e05ef17a2e8e159e3d690fd9128112561
Allie Soldau <alliesoldau@gmail.com>
2022-11-02 17:34:42 -0400

    final submit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee = {
+    name: "Allie",
+    streetAddress: "85 Forest Ave"
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...employee};
+    
+    delete newObj[key];
+
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
